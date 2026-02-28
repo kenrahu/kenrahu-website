8bf7883082b8795231b7513ada0b99568628b6c6
Rahul Kendale <rahulkendale@Rahuls-MacBook-Air.local>
2026-03-01 00:02:03 +0530

    Add PRD Generator route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Quiz from './pages/Quiz'
+import PRDGenerator from './pages/PRDGenerator'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/quiz" element={<Quiz />} />
+        <Route path="/tools/prd-generator" element={<PRDGenerator />} />
       </Routes>
     </BrowserRouter>
   )
